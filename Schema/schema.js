49b9991a1d7fe484e89d395d4b2e50aea3ea054d
Daisky <nakamura.daisky@gmail.com>
2020-08-29 02:37:44 +0900

    AuthorType RootQuery Mutationを定義

---
+++ Schema/schema.js
@@ -0,0 +1,64 @@
+const graphql = require('graphql')
+const Author = require('../Models/author')
+
+const {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLSchema
+} = graphql
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+  }),
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    author: {
+      type: AuthorType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        return Author.findById(args.id)
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve() {
+        return Author.find({})
+      }
+    }
+  }
+})
+
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        const author = new Author({
+          name: args.name
+        })
+        return author.save()
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation
+})
